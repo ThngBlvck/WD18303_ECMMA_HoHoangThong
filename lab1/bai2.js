6a41cb7bb6dcaacab002b978f4a2ddf5c1e2d6e6
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-01-05 08:23:21 +0700

    Bai 2 - Lab 1

---
+++ lab1/bai2.js
@@ -0,0 +1,11 @@
+//
+let name = "Hoang Thong";
+let birthday ="200x/01/23" ;
+
+// arow function
+let sayHello = function() {
+    console.log(`I am ${name} and ${birthday}`);
+}
+// Tinh thoi gian ton tai 
+sayHello();
+
