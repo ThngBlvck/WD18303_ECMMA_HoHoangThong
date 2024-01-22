2f34ecf9da0153dc1b4a09d899d370ce2606560f
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-01-22 19:59:19 +0700

    ASM 1

---
+++ ASM/assets/js/main.js
@@ -0,0 +1,30 @@
+let hot = data.filter(function (item) {
+    return item.hotdeal == 1;
+})
+console.log(hot);
+
+let cate_hot = document.querySelector("#Ghe");
+
+hot.forEach(function (item) {
+    cate_hot.innerHTML += `
+    
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="shop-single.html">
+                            <img src="./assets/img/${item.image}" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <ul class="list-unstyled d-flex justify-content-between">
+                                <li class="text-muted text-right"> ${item.price}đ</li>
+                            </ul>
+                            <a href="shop-single.html" class="h2 text-decoration-none text-dark color-dot-black">${item.name}</a>
+                            <p class="card-text">
+                               ${item.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            
+    `
+});
